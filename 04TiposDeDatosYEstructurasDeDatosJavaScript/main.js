783c25a7bf464afd2ccf2b35cb601e016ac5284d
Dixon G <dxxn0101@gmail.com>
2022-09-15 10:26:43 -0600

    Learning JS | Tipos de datos y estructuras de datos JavaScript | Escritura dinámica

--- 04TiposDeDatosYEstructurasDeDatosJavaScript/main.js
+++ 04TiposDeDatosYEstructurasDeDatosJavaScript/main.js
@@ -1,3 +1,19 @@
 // Tipos de datos y estructuras de datos JavaScript #ff0
 
 // Todos los lenguajes de programación tienen estructuras de datos incorporadas, pero a menudo difieren de un lenguaje a otro. En este artículo se intenta enumerar las estructuras de datos integradas disponibles en JavaScript y las propiedades que tienen. Estos se pueden utilizar para construir otras estructuras de datos. Siempre que es posible, se hacen comparaciones con otros idiomas.
+
+// Escritura dinámica #008000
+
+// JavaScript es un lenguaje dinámico y de tipo libre. Las variables en JavaScript no están directamente asociadas con ningún tipo de valor en particular, y a cualquier variable se le pueden asignar (y reasignar) valores de todos los tipos:
+
+let foo = 42; //  foo es ahora un número
+
+console.log(foo);
+
+foo = "Bar"; // foo es ahora una cadena
+
+console.log(foo);
+
+foo = true; // foo es ahora un booleano
+
+console.log(foo);
