16a1ac4a17f51590119e63e63a69ce2ad5b8edde
Dixon G <dxxn0101@gmail.com>
2022-09-15 10:19:53 -0600

    Learning JS | Tipos de datos y estructuras de datos JavaScript

---
+++ 04TiposDeDatosYEstructurasDeDatosJavaScript/main.js
@@ -0,0 +1,3 @@
+// Tipos de datos y estructuras de datos JavaScript #ff0
+
+// Todos los lenguajes de programación tienen estructuras de datos incorporadas, pero a menudo difieren de un lenguaje a otro. En este artículo se intenta enumerar las estructuras de datos integradas disponibles en JavaScript y las propiedades que tienen. Estos se pueden utilizar para construir otras estructuras de datos. Siempre que es posible, se hacen comparaciones con otros idiomas.
