6912efe1cf1358b98f1cbe0951b84f72c5663124
Dixon G <dxxn0101@gmail.com>
2022-09-14 18:11:42 -0600

    Learning JS | Hoisting | Hoisting de funciones

--- 02Hoisting/main.js
+++ 02Hoisting/main.js
@@ -5,3 +5,23 @@
 
 // Las declaraciones de variables y clases también se elevan, por lo que también se puede hacer referencia a ellas antes de que se declaren. Tenga en cuenta que hacerlo puede provocar errores inesperados y, en general, no se recomienda.
 
+// Hoisting de funciones #008000
+// Una de las ventajas de la elevación es que le permite usar una función antes de declararla en su código.
+
+catName("Tiger");
+
+function catName(name) {
+  console.log(`My cat's name is ${name}`);
+}
+
+// El resultado del código anterior es: "Mi gato se llama Tigre"
+
+// Sin hoisting tendrías que escribir el mismo código así:
+
+function catName2(name) {
+  console.log(`My cat's name is ${name}`);
+}
+
+catName2("Tom");
+
+// El resultado del código anterior es: "Mi gato se llama Tom"
