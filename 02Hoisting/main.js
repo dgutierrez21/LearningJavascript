ca7ca85bf6d44020b66ee3b5a2840efb6c3abbde
Dixon G <dxxn0101@gmail.com>
2022-09-14 18:05:22 -0600

    Learning JS | Hoisting | Hoisting

---
+++ 02Hoisting/main.js
@@ -0,0 +1,7 @@
+// Hoisting #ff0
+// JavaScript Hoisting se refiere al proceso por el cual el intérprete parece mover la declaración de funciones, variables o clases a la parte superior de su ámbito, antes de la ejecución del código.
+
+// La elevación permite que las funciones se utilicen de forma segura en el código antes de que se declaren.
+
+// Las declaraciones de variables y clases también se elevan, por lo que también se puede hacer referencia a ellas antes de que se declaren. Tenga en cuenta que hacerlo puede provocar errores inesperados y, en general, no se recomienda.
+
