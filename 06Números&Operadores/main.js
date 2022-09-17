0c76064d45a976912d6d87824dd730e3e158c3a9
Dixon G <dxxn0101@gmail.com>
2022-09-17 13:29:36 -0600

    Learning JS | Números y operadores

---
+++ 06Números&Operadores/main.js
@@ -0,0 +1,5 @@
+// A todo el mundo le encantan las matemáticas #008000
+// De acuerdo, tal vez no. A algunos de nosotros nos gustan las matemáticas, algunos de nosotros hemos odiado las matemáticas desde que tuvimos que aprender tablas de multiplicar y división larga en la escuela, y algunos de nosotros nos sentamos en algún lugar entre los dos. Pero ninguno de nosotros puede negar que las matemáticas son una parte fundamental de la vida sin la que no podemos llegar muy lejos. Esto es especialmente cierto cuando estamos aprendiendo a programar JavaScript (o cualquier otro lenguaje para el caso): gran parte de lo que hacemos se basa en el procesamiento de datos numéricos, el cálculo de nuevos valores, etc., que no se sorprenderá al saber que JavaScript tiene un conjunto completo de funciones matemáticas disponibles.
+
+// Este artículo discute solo las partes básicas que necesita saber ahora.
+
